5878d594e763d4e7b992d580775de42a7e973202
Anjan Roy <anjanroy@yandex.com>
2022-01-06 03:53:28 +0000

    random input to hash function with 1024 chunks (i.e. 1MB input to hash function)

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,6 +1,7 @@
-#include "utils.hpp"
+#include "blake3.hpp"
 #include <cassert>
 #include <iostream>
+#include <random>
 
 extern "C" uint8_t*
 rust_blake3(uint8_t* input, uint64_t i_size, uint8_t* digest);
@@ -11,12 +12,20 @@ main()
   sycl::device d{ sycl::default_selector{} };
   sycl::queue q{ d };
 
-  const size_t chunk_count = 2;
+  const size_t chunk_count = 1 << 10;
   const size_t i_size = chunk_count * blake3::CHUNK_LEN;
 
   uint8_t* in = static_cast<uint8_t*>(malloc(i_size));
   uint8_t* out = static_cast<uint8_t*>(malloc(blake3::OUT_LEN));
-  prepare_blake3_input(chunk_count, in);
+
+  {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<uint8_t> dis(0, 255);
+
+    // setting up input to hash function
+    memset(in, dis(gen), i_size);
+  }
 
   sycl::uchar* in_d = static_cast<sycl::uchar*>(sycl::malloc_device(i_size, q));
   sycl::uchar* out_h =
@@ -28,9 +37,9 @@ main()
   q.memcpy(in_d, in, i_size).wait();
 
   uint8_t* digest = rust_blake3(in, i_size, out);
-
   blake3::hash(q, in_d, i_size, chunk_count, chunk_count, out_d);
-  // copy output digest into host and wait
+
+  // copy output digest to host and wait until completed !
   q.memcpy(out_h, out_d, blake3::OUT_LEN).wait();
 
   for (size_t i = 0; i < blake3::OUT_LEN; i++) {
