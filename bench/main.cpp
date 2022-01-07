891c64531706d8d0b57ab6e622e9cd7b7c88ad5d
Anjan Roy <anjanroy@yandex.com>
2022-01-07 09:16:07 +0000

    benchmark second implementation of blake3

--- bench/main.cpp
+++ bench/main.cpp
@@ -2,6 +2,9 @@
 #include <iomanip>
 #include <iostream>
 
+std::string
+to_readable_timespan(sycl::ulong ts);
+
 int
 main(int argc, char** argv)
 {
@@ -16,24 +19,44 @@ main(int argc, char** argv)
 
   const size_t wg_size = 1 << 6;
 
-  std::cout << "Benchmarking BLAKE3 SYCL implementation" << std::endl
+  std::cout << "Benchmarking BLAKE3 SYCL implementation (v1)" << std::endl
             << std::endl;
   std::cout << std::setw(24) << std::right << "input size"
             << "\t\t" << std::setw(16) << std::right << "execution time"
             << std::endl;
 
   for (size_t i = 1 << 10; i <= 1 << 20; i <<= 1) {
-    sycl::cl_ulong ts = benchmark_blake3(q, i, wg_size);
-    std::string ts_s =
-      ts >= 1e9 ? std::to_string(ts * 1e-9) + " s"
-                : ts >= 1e6 ? std::to_string(ts * 1e-6) + " ms"
-                            : ts >= 1e3 ? std::to_string(ts * 1e-3) + " us"
-                                        : std::to_string(ts) + " ns";
+    sycl::cl_ulong ts = benchmark_blake3_v1(q, i, wg_size);
 
     std::cout << std::setw(20) << std::right << ((i * blake3::CHUNK_LEN) >> 20)
               << " MB"
-              << "\t\t" << std::setw(22) << std::right << ts_s << std::endl;
+              << "\t\t" << std::setw(22) << std::right
+              << to_readable_timespan(ts) << std::endl;
+  }
+
+  std::cout << "\nBenchmarking BLAKE3 SYCL implementation (v2)" << std::endl
+            << std::endl;
+  std::cout << std::setw(24) << std::right << "input size"
+            << "\t\t" << std::setw(16) << std::right << "execution time"
+            << std::endl;
+
+  for (size_t i = 1 << 10; i <= 1 << 20; i <<= 1) {
+    sycl::cl_ulong ts = benchmark_blake3_v2(q, i, wg_size);
+
+    std::cout << std::setw(20) << std::right << ((i * blake3::CHUNK_LEN) >> 20)
+              << " MB"
+              << "\t\t" << std::setw(22) << std::right
+              << to_readable_timespan(ts) << std::endl;
   }
 
   return 0;
+}
+
+std::string
+to_readable_timespan(sycl::ulong ts)
+{
+  return ts >= 1e9 ? std::to_string(ts * 1e-9) + " s"
+                   : ts >= 1e6 ? std::to_string(ts * 1e-6) + " ms"
+                               : ts >= 1e3 ? std::to_string(ts * 1e-3) + " us"
+                                           : std::to_string(ts) + " ns";
 }
--- include/bench_blake3.hpp
+++ include/bench_blake3.hpp
@@ -3,7 +3,7 @@
 #include <random>
 
 sycl::cl_ulong
-benchmark_blake3(sycl::queue& q, size_t chunk_count, size_t wg_size)
+benchmark_blake3_v1(sycl::queue& q, size_t chunk_count, size_t wg_size)
 {
   // current implementation of blake3 only works
   // with power of 2 -number of chunks
@@ -31,6 +31,45 @@ benchmark_blake3(sycl::queue& q, size_t chunk_count, size_t wg_size)
 
   q.memcpy(i_d, i_h, i_size).wait();
   blake3::v1::hash(q, i_d, i_size, chunk_count, wg_size, o_d, &ts);
+  q.memcpy(o_h, o_d, o_size).wait();
+
+  sycl::free(i_h, q);
+  sycl::free(i_d, q);
+  sycl::free(o_h, q);
+  sycl::free(o_d, q);
+
+  return ts;
+}
+
+sycl::cl_ulong
+benchmark_blake3_v2(sycl::queue& q, size_t chunk_count, size_t wg_size)
+{
+  // current implementation of blake3 only works
+  // with power of 2 -number of chunks
+  assert((chunk_count & (chunk_count - 1)) == 0);
+  assert(wg_size <= chunk_count);
+
+  const size_t i_size = chunk_count * blake3::CHUNK_LEN; // in bytes
+  const size_t o_size = blake3::OUT_LEN;                 // in bytes
+
+  sycl::uchar* i_h = static_cast<sycl::uchar*>(sycl::malloc_host(i_size, q));
+  sycl::uchar* i_d = static_cast<sycl::uchar*>(sycl::malloc_device(i_size, q));
+
+  sycl::uchar* o_h = static_cast<sycl::uchar*>(sycl::malloc_host(o_size, q));
+  sycl::uchar* o_d = static_cast<sycl::uchar*>(sycl::malloc_device(o_size, q));
+
+  {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<uint8_t> dis(0, 255);
+
+    memset(i_h, dis(gen), i_size); // prepare (random) input
+  }
+
+  sycl::cl_ulong ts = 0; // timing info, ensure queue has profiling enabled
+
+  q.memcpy(i_d, i_h, i_size).wait();
+  blake3::v2::hash(q, i_d, i_size, chunk_count, wg_size, o_d, &ts);
   q.memcpy(o_h, o_d, o_size).wait();
 
   sycl::free(i_h, q);
