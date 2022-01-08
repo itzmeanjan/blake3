31ecad5b3ca572fe87b9d64a2c302ec2d627c8b8
Anjan Roy <anjanroy@yandex.com>
2022-01-08 13:27:24 +0000

    when benchmarking take average by executing each input combination N times; here I set N = 4

--- bench/main.cpp
+++ bench/main.cpp
@@ -2,8 +2,21 @@
 #include <iomanip>
 #include <iostream>
 
+enum BLAKE3_VARIANT
+{
+  V1,
+  V2
+};
+
+sycl::cl_ulong
+avg_kernel_exec_tm(sycl::queue& q,
+                   size_t chunk_count,
+                   size_t wg_size,
+                   size_t itr_cnt,
+                   BLAKE3_VARIANT variant);
+
 std::string
-to_readable_timespan(sycl::ulong ts);
+to_readable_timespan(sycl::cl_ulong ts);
 
 int
 main(int argc, char** argv)
@@ -17,7 +30,8 @@ main(int argc, char** argv)
             << std::endl
             << std::endl;
 
-  const size_t wg_size = 1 << 6;
+  const size_t wg_size = 1 << 5;
+  const size_t itr_cnt = 1 << 2;
 
   std::cout << "Benchmarking BLAKE3 SYCL implementation (v1)" << std::endl
             << std::endl;
@@ -26,12 +40,12 @@ main(int argc, char** argv)
             << std::endl;
 
   for (size_t i = 1 << 10; i <= 1 << 20; i <<= 1) {
-    sycl::cl_ulong ts = benchmark_blake3_v1(q, i, wg_size);
+    std::string ts = to_readable_timespan(
+      avg_kernel_exec_tm(q, i, wg_size, itr_cnt, BLAKE3_VARIANT::V1));
 
     std::cout << std::setw(20) << std::right << ((i * blake3::CHUNK_LEN) >> 20)
               << " MB"
-              << "\t\t" << std::setw(22) << std::right
-              << to_readable_timespan(ts) << std::endl;
+              << "\t\t" << std::setw(22) << std::right << ts << std::endl;
   }
 
   std::cout << "\nBenchmarking BLAKE3 SYCL implementation (v2)" << std::endl
@@ -41,19 +55,41 @@ main(int argc, char** argv)
             << std::endl;
 
   for (size_t i = 1 << 10; i <= 1 << 20; i <<= 1) {
-    sycl::cl_ulong ts = benchmark_blake3_v2(q, i, wg_size);
+    std::string ts = to_readable_timespan(
+      avg_kernel_exec_tm(q, i, wg_size, itr_cnt, BLAKE3_VARIANT::V2));
 
     std::cout << std::setw(20) << std::right << ((i * blake3::CHUNK_LEN) >> 20)
               << " MB"
-              << "\t\t" << std::setw(22) << std::right
-              << to_readable_timespan(ts) << std::endl;
+              << "\t\t" << std::setw(22) << std::right << ts << std::endl;
   }
 
   return 0;
 }
 
+sycl::cl_ulong
+avg_kernel_exec_tm(sycl::queue& q,
+                   size_t chunk_count,
+                   size_t wg_size,
+                   size_t itr_cnt,
+                   BLAKE3_VARIANT variant)
+{
+  sycl::cl_ulong ts = 0;
+
+  for (size_t i = 0; i < itr_cnt; i++) {
+    if (variant == BLAKE3_VARIANT::V1) {
+      ts += benchmark_blake3_v1(q, chunk_count, wg_size);
+    } else if (variant == BLAKE3_VARIANT::V2) {
+      ts += benchmark_blake3_v2(q, chunk_count, wg_size);
+    } else {
+      throw "can't benchmark unsupported BLAKE3 variant";
+    }
+  }
+
+  return ts / static_cast<sycl::cl_ulong>(itr_cnt);
+}
+
 std::string
-to_readable_timespan(sycl::ulong ts)
+to_readable_timespan(sycl::cl_ulong ts)
 {
   return ts >= 1e9 ? std::to_string(ts * 1e-9) + " s"
                    : ts >= 1e6 ? std::to_string(ts * 1e-6) + " ms"
