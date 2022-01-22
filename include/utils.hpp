5e4747661201f6f8ca4f9ec1d43719d11fb0c24f
Anjan Roy <anjanroy@yandex.com>
2022-01-22 06:46:03 +0000

    generic utility function to be executed on host which helps in converting between little endian bytes array and blake3 words

--- include/utils.hpp
+++ include/utils.hpp
@@ -26,3 +26,44 @@ time_event(sycl::event& evt)
 
   return (end - start);
 }
+
+void
+words_from_le_bytes(const sycl::uchar* input,
+                    size_t i_size,
+                    sycl::uint* const msg_words,
+                    size_t m_cnt)
+{
+  // because each message word of BLAKE3 is of 4 -bytes
+  assert(i_size == m_cnt << 2);
+
+#pragma unroll 8 // partial unrolling !
+  for (size_t i = 0; i < m_cnt; i++) {
+    const sycl::uchar* i_start = (input + i * 4);
+
+    *(msg_words + i) = (static_cast<sycl::uint>(*(i_start + 3)) << 24) |
+                       (static_cast<sycl::uint>(*(i_start + 2)) << 16) |
+                       (static_cast<sycl::uint>(*(i_start + 1)) << 8) |
+                       (static_cast<sycl::uint>(*(i_start + 0)) << 0);
+  }
+}
+
+void
+words_to_le_bytes(const sycl::uint* msg_words,
+                  size_t m_cnt,
+                  sycl::uchar* const output,
+                  size_t o_size)
+{
+  // because each message word of BLAKE3 is of 4 -bytes
+  assert(o_size == m_cnt << 2);
+
+#pragma unroll 8 // partial unrolling !
+  for (size_t i = 0; i < m_cnt; i++) {
+    const sycl::uint num = *(msg_words + i);
+    sycl::uchar* out = output + (i << 2);
+
+    *(out + 0) = static_cast<sycl::uchar>((num >> 0) & 0xff);
+    *(out + 1) = static_cast<sycl::uchar>((num >> 8) & 0xff);
+    *(out + 2) = static_cast<sycl::uchar>((num >> 16) & 0xff);
+    *(out + 3) = static_cast<sycl::uchar>((num >> 24) & 0xff);
+  }
+}
