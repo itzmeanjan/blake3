e2a1340a9a7972854889d494b450d72c2198cace
Anjan Roy <anjanroy@yandex.com>
2022-01-22 15:35:56 +0000

    added some comments

--- include/merklize.hpp
+++ include/merklize.hpp
@@ -12,27 +12,43 @@ merklize(sycl::queue& q,
          size_t wg_size)
 {
   // A binary merkle tree with N -many leaf
-  // nodes should have N-1 -many intermediates
+  // nodes should have (N - 1) -many intermediates
   //
   // Note N = power of 2
   assert(leaf_cnt == itmd_cnt + 1);
 
+  // because each leaf node is 32 -bytes wide
   assert(i_size == (leaf_cnt << 5));
+  // if a binary tree has N -many leaf nodes, where N = 2 ^ i | i = {1, 2, ...}
+  // then # -of intermediate nodes in that binary merkle tree should be =
+  // leaf_count - 1
   assert(o_size == ((itmd_cnt + 1) << 5));
+  // both input and output allocation has same size
   assert(i_size == o_size);
 
   // only tree with power of 2 many leaf nodes
-  // can be merklized
+  // can be merklized by this implementation
   assert((leaf_cnt & (leaf_cnt - 1)) == 0);
 
+  // At first N -many leaf nodes to be merged into
+  // N/ 2 -many intermediate nodes, which are living
+  // just above leaf nodes
   const size_t work_item_cnt = leaf_cnt >> 1;
+
+  // validate whether this work group size can be used for next kernel dispatch
   assert(wg_size <= work_item_cnt);
+  // ensure all dispatched work groups have equal many
+  // active work-items
   assert(work_item_cnt % wg_size == 0);
 
+  // # -of 32 -bit unsigned integers, which can be contiguously placed
+  // on output memory allocation
   const size_t elm_cnt = o_size >> 2;
   const size_t i_offset = 0;
   const size_t o_offset = elm_cnt >> 1;
 
+  // computes all intermediate nodes which are living just above leaf nodes of
+  // binary merkle tree
   sycl::event evt_0 = q.submit([&](sycl::handler& h) {
     h.parallel_for<class kernelBinaryMerklizationUsingBLAKE3Phase0>(
       sycl::nd_range<1>{ sycl::range<1>{ work_item_cnt },
@@ -54,6 +70,7 @@ merklize(sycl::queue& q,
       });
   });
 
+  // these many kernel dispatch rounds still remaining
   const size_t rounds =
     static_cast<size_t>(sycl::log2(static_cast<double>(work_item_cnt)));
 
@@ -68,7 +85,13 @@ merklize(sycl::queue& q,
   evts_0.push_back(evt_0);
 
   for (size_t r = 0; r < rounds; r++) {
+    // multiple rounds of kernel dispatches, where intermediate nodes are being
+    // computed from already computed (in previous dispatch round) intermediate
+    // nodes
     sycl::event evt_1 = q.submit([&](sycl::handler& h) {
+      // note dependency chain being built !
+      //
+      // each dispatch round depends on previously enqueued dispatch round
       h.depends_on(evts_0.at(r));
 
       const size_t work_item_cnt_ = work_item_cnt >> (r + 1);
@@ -100,6 +123,8 @@ merklize(sycl::queue& q,
     evts_0.push_back(evt_1);
   }
 
+  // wait for last kernel dispatch round, where root of binary merkle tree is
+  // computed !
   evts_0.at(rounds).wait();
 
   // time execution of all enqueued kernels with nanosecond level granularity
